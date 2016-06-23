aaf657ae266c460a6caffe6551e910fe7049a075
tjivancic <tjivancic@gmail.com>
2016-06-23 13:00:41 -0400

    Update stormbuilderdemo.py

--- stormbuilderdemo.py
+++ stormbuilderdemo.py
@@ -1,5 +1,6 @@
 import stormbuilder as sb
-
+import os
+os.system('tar -zxvf OnionCreekJun13.tar.gz')
 P = sb.read_from_ncfolder('OnionCreekJun13/')
 VP = sb.netvalue(P, weight_mask=1)
 NP = sb.countcells(P)
